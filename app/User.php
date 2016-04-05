425dac93a74d3bab91f0672732362cf82f8fc363
ごま <gomayumax@users.noreply.github.com>
2016-04-05 21:41:41 +0900

    テーブル追加メソッド追加

--- app/User.php
+++ app/User.php
@@ -23,4 +23,9 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+  protected $table = 'users';
+    public function tweet() {
+      return $this->hasMany('App\Tweet');
+    }
 }
